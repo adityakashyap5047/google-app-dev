182e5b13303ec8453a99471ff8f94c592eed649a
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-03 05:40:58 +0530

    exercise 1 solved

---
+++ gadExercise01.kt
@@ -0,0 +1,58 @@
+/** Exercise 1.1 */
+
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+}
+ 
+
+
+/** Exercise 1.2 */
+
+fun main() {
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+}
+
+
+/** Exercise 1.3 */
+
+fun main() {
+    println("Monday") 
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
+
+/** Exercise 1.4 */
+
+fun main() {
+    println("Tomorrow is rainy")
+}
+
+
+/** Exercise 1.5 */
+
+fun main() {
+    println("There is a chance of snow")
+}
+
+
+/** Exercise 1.6 */
+
+fun main() {
+    println("Cloudy") 
+    println("Partly Cloudy") 
+    println("Windy")
+}
+
+
+/** Exercise 1.7 */
+
+fun main() {
+    println("How's the weather today?")
+}
